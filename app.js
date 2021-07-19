33d8d94253a8ee9d8bbfa7a908738c8d9f4993f5
sudhakarhue <82567301+sudhakarhue@users.noreply.github.com>
2021-07-19 18:26:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+var AWS = require('aws-sdk');
+
+app.get('/', (req, res) => {
+
+    // console.log("Message = " + req.query.message);
+    // console.log("Number = " + req.query.number);
+    // console.log("Subject = " + req.query.subject);
+    
+    // var paramsp = {
+    //     Message: req.query.message,
+    //     PhoneNumber: '+' + req.query.number,
+    //     //TopicArn:'arn:aws:sns:us-east-1:862163474169:MyFirstTopic',
+    //     MessageAttributes: {
+    //         'AWS.SNS.SMS.SenderID': {
+    //             'DataType': 'String',
+    //             'StringValue': req.query.subject
+    //         }
+    //     }
+    // };
+   
+    //   var snsIns = new AWS.SNS({ apiVersion: '2010-03-31' })
+    //         // successful response
+    //         var publishTextPromise = snsIns.publish(paramsp).promise();
+    //         publishTextPromise.then(
+    //             function (data) { 
+    //                 console.log("data.message  = " + data.message);
+    //                 console.log("data.PhoneNumber = " + data.PhoneNumber);
+                    
+    //                 console.log("req.query.message = " + req.query.message);
+    //                   res.end(JSON.stringify({ MessageID: data.MessageId }));
+    //             }).catch(
+    //                 function (err) {
+    //                     res.end(JSON.stringify({ Error: err }));
+    //                 });  
+    
+    
+    
+
+    const client = require('twilio')(
+        'AC35c18346fa0a86db122669f9a1e4b8fe',
+        'e5c99e2d50c207464adc688d7b336d14' 
+      );
+      
+      client.messages.create({
+        from: '+14086062860',
+        to: '+919840391870',
+        body: "You just sent an SMS from Node.js using Twilio!"
+      }).then(
+                    function (data) { 
+                        console.log("data.accountSid  = " + data.accountSid);
+                        console.log("data.body = " + data.body);                        
+                        
+                          res.end(JSON.stringify({ MessageID: data.messagingServiceSid }));
+                    }).catch(
+                        function (err) {
+                            res.end(JSON.stringify({ Error: err }));
+                        });  
+        
+
+                    }); 
+app.listen(3000, () => console.log('SMS Service Listening on PORT 3000'))
